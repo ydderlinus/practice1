e33e2a2ce03f828727a71f5a22f96305443e205d
ydderlinus <ydderlinus@gmail.com>
2025-04-09 11:45:50 +0530

    added python files

---
+++ app.py
@@ -0,0 +1,7 @@
+# app.py
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(add(2, 3))
+
---
+++ test_app.py
@@ -0,0 +1,7 @@
+# test_app.py
+from app import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
